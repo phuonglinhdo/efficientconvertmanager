258ed030c8f9f599711c75d756cefdc0146f5a80
phuonglinhdo <dasilvaa38pun0suy@hotmail.com>
2024-05-08 03:50:35 +0700

    hello

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-function rotateRight(head, k) {
-  if (!head) return null;
-  let length = 1;
-  let tail = head;
-  while (tail.next) {
-    length++;
-    tail = tail.next;
-  }
-  k %= length;
-  if (k === 0) return head;
-  let newTail = head;
-  for (let i = 0; i < length - k - 1; i++) {
-    newTail = newTail.next;
-  }
-  const newHead = newTail.next;
-  newTail.next = null;
-  tail.next = head;
-  return newHead;
-}
+const message = "Hello world!";
+const messageLength = message.length;
+console.log(messageLength); // 13
