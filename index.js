5ed212b87e1bff82f2321856da4a6ed7aae81363
phuonglinhdo <dasilvaa38pun0suy@hotmail.com>
2024-05-08 11:39:05 +0700

    add more features

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const message = "Hello world!";
-const messageLength = message.length;
-console.log(messageLength); // 13
+function isValidParentheses(s) {
+  const stack = [];
+  const map = { "(": ")", "[": "]", "{": "}" };
+  for (const char of s) {
+    if (char in map) stack.push(char);
+    else {
+      const top = stack.pop();
+      if (map[top] !== char) return false;
+    }
+  }
+  return stack.length === 0;
+}
