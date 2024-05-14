b4a4bdfa9289881bf0a1b778f6fe73654729266a
phuonglinhdo <dasilvaa38pun0suy@hotmail.com>
2024-05-14 18:06:52 +0700

    demo

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const pullAtIndex = (arr, pullArr) => {
-  let removed = [];
-  let pulled = arr
-    .map((v, i) => (pullArr.includes(i) ? removed.push(v) : v))
-    .filter((v, i) => !pullArr.includes(i));
-  arr.length = 0;
-  pulled.forEach((v) => arr.push(v));
-  return removed;
-};
+function reverseList(head) {
+  let prev = null;
+  let curr = head;
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+}
